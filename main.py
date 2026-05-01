4fbb77d82b613cc6113230e307dcc7756548e3f0
Weronika83 <weronika.szelag@student.wat.edu.pl>
2026-05-01 21:33:36 +0200

    wip: complex data structure

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-zmienna_1: str = 'oliwia'
-zmienna_2: str = 'paweł'
-zmienna_3: str = 'eliza'
-zmienna_4: str = 'filip'
-zmienna_5: str = 'ewelina'
-zmienna_6: str = 'weronika'
+users: list=[
+    {'username':'oliwia', 'location':'łódź','posts':1,'usermessage':['życzenia1','kocham legie', 'sprzeam opla', 'kiwi1']},
+    {'username':'paweł', 'location':'ostróda','posts':2,'usermessage':['życzenia2','kocham legie1', 'sprzeam opla1']},
+    {'username':'eliza', 'location':'raom','posts':3,'usermessage':['życzenia3','kocham legie2']},
+    {'username': 'filip', 'location': 'dęblin','posts':4, 'usermessage':['życzenia4','kocham legie3', 'sprzeam opla3', 'kiwi3']},
+]
 
-users: list=[zmienna_1, zmienna_2, zmienna_3, zmienna_4, zmienna_5, zmienna_6]
-
-for user in users:
-    print(user)
+for user in users[1:]:
+    print(f'twój znajomy {user['username']} z miejscowości {user['location']} opublikował {user['posts']} wiadomości. Ostatnia wiaomość {user['usermessage'][-1]}')
